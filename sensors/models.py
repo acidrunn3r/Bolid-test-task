ff45061c6ebe7826b1defebdc01679e9146acb07
Alexander Lipov <lipov.sa@mail.ru>
2025-09-27 14:15:57 -0700

    Fix sensor id being negative
    До фикса код позволял при загрузке из json-файла события, у которого sensor_id < 0, все равно добавлять событие в базу данных, что некорректно.

--- sensors/models.py
+++ sensors/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Sensor(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.IntegerField(primary_key=True, validators=[MinValueValidator(1)])
     name = models.CharField(
         max_length=50,
         validators=[
