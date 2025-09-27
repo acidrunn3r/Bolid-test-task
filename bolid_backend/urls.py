5b3bfd5c508b54ab8201b4d6ce29ff800517b33b
Alexander Lipov <lipov.sa@mail.ru>
2025-09-27 13:40:11 -0700

    Add redirect for root address

--- bolid_backend/urls.py
+++ bolid_backend/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 from rest_framework.routers import DefaultRouter
 
 from sensors.views import EventViewSet, SensorViewSet
@@ -9,6 +10,7 @@ router.register("sensors", SensorViewSet)
 router.register("events", EventViewSet)
 
 urlpatterns = [
+    path("", RedirectView.as_view(url="/api/<version>/", permanent=False)),
     path("admin/", admin.site.urls),
     path("api/<version>/", include(router.urls)),
 ]
